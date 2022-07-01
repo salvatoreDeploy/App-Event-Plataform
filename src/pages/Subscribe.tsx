b9708c7a51cb817ff99cb2ce089f67530e866299
salvatoreDeploy <liderhenrique@gmail.com>
2022-07-01 20:26:13 -0300

    Alteração na Imagem Subscriber

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import mockupImg from "../assets/code-mockup.png";
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -72,7 +73,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={mockupImg} className="mt-10" alt="" />
     </div>
   );
 }
